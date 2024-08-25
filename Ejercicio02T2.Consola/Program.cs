7e57ce4105f24bedcc70f4ce5e15f9487f3edd52
DanyMarhon <danymarhon2015@gmail.com>
2024-08-25 14:28:25 -0300

    Ajuste Ejercicio 2T2

--- Ejercicio02T2.Consola/Program.cs
+++ Ejercicio02T2.Consola/Program.cs
@@ -7,9 +7,22 @@ namespace Ejercicio02T2.Consola
     {
         static void Main(string[] args)
         {
-            Console.Write("Ingrese la patente:");
-            string patente = Console.ReadLine();
+            Console.Write("\nEjemplo patente nueva:  ");
+            string patente = "AR 123 GG";
             Console.WriteLine(ValidadorPatente.InformarPatente(patente));
+
+            Console.Write("\nEjemplo patente vieja:  ");
+            string patente2 = "ARG 123";
+            Console.WriteLine(ValidadorPatente.InformarPatente(patente2));
+
+            Console.Write("\nEjemplo patente invalida:  ");
+            string patente3 = "Joder123";
+            Console.WriteLine(ValidadorPatente.InformarPatente(patente3));
+
+            Console.Write("\nRealiza una prueba \n");
+            Console.Write("Ingrese la patente:  ");
+            string patente4 = Console.ReadLine()!;
+            Console.WriteLine(ValidadorPatente.InformarPatente(patente4));
             ConsoleExtensions.EsperarPresionDeTecla(" ");
         }
     }
--- Ejercicio02T2.Entidades/ValidadorPatente.cs
+++ Ejercicio02T2.Entidades/ValidadorPatente.cs
@@ -7,8 +7,8 @@ namespace Ejercicio02T2.Entidades
     {
         private static string formato1 = @"^[A-Z]{3} \d{3}$";   // AAA NNN
         private static string formato2 = @"^[A-Z]{2} \d{3} [A-Z]{2}$"; // AA NNN AA
-        private static string messageNueva = "La patente es nueva";
-        private static string messageVieja= "La patente es vieja";
+        private static string messageNueva = " posee el formato nuevo";
+        private static string messageVieja= " posee el formato viejo";
 
         public static bool Validar(string patente)
         {
@@ -24,7 +24,7 @@ namespace Ejercicio02T2.Entidades
             
             if (Regex.IsMatch(patente, formato1)) return $"La patente {patente} {messageVieja}";
             if (Regex.IsMatch(patente, formato2)) return $"La patente {patente} {messageNueva}";
-            else { return $"El número {patente} no es una patente"; }
+            else { return $"El número {patente} no corresponde a una patente"; }
         }
     }
 
