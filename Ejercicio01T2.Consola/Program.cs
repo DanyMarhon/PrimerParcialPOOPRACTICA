3ea412baf8422824349d21e26bfd89a0a1c0b9d7
DanyMarhon <danymarhon2015@gmail.com>
2024-08-25 16:15:40 -0300

    Ejercicio 1 T 2 realizado

--- Ejercicio01T2.Consola/Program.cs
+++ Ejercicio01T2.Consola/Program.cs
@@ -1,11 +1,17 @@
-﻿namespace Ejercicio01T2.Consola
+﻿using Ejercicio01T2.Entidades;
+using PrimerParcialPOO2024Tema1.Shared;
+
+namespace Ejercicio01T2.Consola
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Cono cono = new Cono(3,5);
+
             Console.WriteLine(cono.GetInfo());
+
+            ConsoleExtensions.EsperarPresionDeTecla(" ");
         }
     }
 }
